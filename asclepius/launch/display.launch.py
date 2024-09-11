8b60a60d0bf754c4304210c7aedbf3ceb4694074
Fernando Matias <fm278841@gmail.com>
2024-09-10 19:30:08 -0700

    refactoring

---
+++ asclepius/launch/display.launch.py
@@ -0,0 +1,55 @@
+
+import os
+from ament_index_python.packages import get_package_share_directory as pkgdir
+from launch import LaunchDescription
+
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+    pkg_name = 'asclepius'
+    file_subpath = 'urdf/asclepius.urdf.xacro'
+    
+    # Locate the RVIZ configuration file.
+    rvizcfg = os.path.join(pkgdir(pkg_name), 'rviz/viewrobot.rviz')
+
+    # Locate xacro file
+    xacro_file = os.path.join(pkgdir(pkg_name),file_subpath)
+    robot_description_config = xacro.process_file(xacro_file).toxml()
+
+
+    joint_node = Node(
+            package="joint_state_publisher_gui",
+            executable="joint_state_publisher_gui",
+            name="joint_state_publisher_gui",
+            output="screen")
+    
+    robot_state_publisher_node = Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            parameters=[
+                {"robot_description": robot_description_config}],
+            output="screen")
+    
+    rviz_node = Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            arguments=["-d", rvizcfg],
+            output="screen")
+    
+    # transform_node = Node(
+    #         name       = 'transform_node', 
+    #         package    = 'dynamixel_arm',
+    #         executable = 'transform_node',
+    #         output     = 'screen') 
+    
+    return LaunchDescription([
+        joint_node,
+        robot_state_publisher_node,
+        rviz_node,
+        # transform_node,
+    ])
---
+++ dynamixel_arm/launch/sim.launch.py
@@ -0,0 +1,94 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+    # Launch arguments
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
+    project_path = os.path.join(
+        get_package_share_directory('dynamixel_arm'))
+
+    xacro_file = os.path.join(project_path,
+                              'urdf',
+                              'acps_bot.urdf.xacro')
+    rvizcfg = os.path.join(get_package_share_directory('dynamixel_arm'), 'rviz/viewrobot.rviz')
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml()}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    ignition_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-string', doc.toxml(),
+                   '-name', 'acps_bot',
+                   '-allow_renaming', 'true'],
+    )
+    rviz_node = Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            arguments=["-d", rvizcfg],
+            output="screen")
+
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_acps_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_trajectory_controller'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        # Launch gazebo environment
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory('ros_gz_sim'),
+                              'launch', 'gz_sim.launch.py')]),
+            launch_arguments=[('ign_args', [' -r -v 4 empty.sdf'])]),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=ignition_spawn_entity,
+                on_exit=[load_joint_state_broadcaster],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_acps_controller],
+            )
+        ),
+        node_robot_state_publisher,
+        ignition_spawn_entity,
+        rviz_node,
+        # Launch Arguments
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='If true, use simulated clock'),
+    ])
